22075f559f8b6bc8e17b7d25ef5944cff6ce0025
ncevallos <nmcevall@gmail.com>
2017-08-17 21:56:11 -0400

    Implemented the bonus

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var Twitter = require('twitter');
 var sourceFile = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var textFile = 'log.txt';
 
 //below parses any arguments that may have been passed through the command line
 var command = process.argv[2];
@@ -31,11 +32,28 @@ var spotify = new Spotify({
   secret: spotKeySecret
 });
 
-//below is all the code to read and execute the commands. It was placed inside of a function, so that if the do command is run, the information from random.txt can be first read and placed into variables and then runCommand can be re-executed using that information.
+//Below is all the code to read and execute the commands. It was placed inside of a function, so that if the do command is run, the information from random.txt can be first read and placed into variables and then runCommand can be re-executed using that information.
+function appendCommand(command){
+  fs.appendFile(textFile, "\nCommand run: " + command + ", \n", function(err) {
+
+  // If an error was experienced we say it.
+  if (err) {
+    console.log(err);
+  }
+
+  // If no error is experienced, we say nothing
+  else {
+  }
+
+});
+}
+
+//The run command function was implemented so to eliminate reoccuring code of appending the command type to log.txt. As far as appending the data that is retrieved by each function, that is still being done in each code condition.
 function runCommand(){
 
 
 if(command === "my-tweets"){
+appendCommand(command);
 
 var params = {ncevUCF: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -44,6 +62,19 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     for(i=0; i<9; i++){
     console.log("Tweet: " + tweets[i].text);
     console.log("Tweeted at: " + tweets[i].created_at);
+      fs.appendFile(textFile, "\n Tweet: "+tweets[i].text + "\n Tweeted at: "+ tweets[i].created_at + "\n", function(err) {
+
+        // If an error was experienced we say it.
+        if (err) {
+          console.log(err);
+        }
+
+        // If no error is experienced, we say nothing
+        else {
+        }
+
+      });
+
     }
   }
 });
@@ -58,21 +89,34 @@ else if(command === "spotify-this-song"){
 		var songName = command2;
 	}
 
+appendCommand(command);
  
 spotify.search({ type: 'track', query: songName }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
  
-console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
-console.log("Song name: " + data.tracks.items[0].name);
-console.log("Album: " + data.tracks.items[0].album.name);
-console.log("URL to Play Song: " + data.tracks.items[0].external_urls.spotify);
+      console.log("Artist(s): " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name + "\nAlbum: " + data.tracks.items[0].album.name + "\nURL to Play Song: " + data.tracks.items[0].external_urls.spotify);
+
+      fs.appendFile(textFile, "\nArtist(s): " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name + "\nAlbum: " + data.tracks.items[0].album.name + "\nURL to Play Song: " + data.tracks.items[0].external_urls.spotify + "\n", function(err) {
+
+        // If an error was experienced we say it.
+        if (err) {
+          console.log(err);
+        }
+
+        // If no error is experienced, we say nothing
+        else {
+        }
+
+});
 });
 	}
 
 
 else if(command === "movie-this"){
+
+appendCommand(command);
 
 		var movieName = "";
 	if(command2 === undefined){
@@ -83,21 +127,30 @@ else if(command === "movie-this"){
 	}
 	    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
         request(queryURL, function (error, response, body) {
- 		  console.log("Title of the Movie is: " + JSON.parse(body).Title);
-          console.log("Year released: " + JSON.parse(body).Year);
-          console.log("IMDB Rating: " + JSON.parse(body).imdbRating); 
-          console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-          console.log("Country Produced:  " + JSON.parse(body).Country); 
-          console.log("Language: " + JSON.parse(body).Language);
-          console.log("Plot: " + JSON.parse(body).Plot); 
-          console.log("Actors: " + JSON.parse(body).Actors);
+ 		  console.log("\nTitle of the Movie is: " + JSON.parse(body).Title + "\nYear released: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry Produced:  " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors );
+
+      fs.appendFile(textFile, "\nTitle of the Movie is: " + JSON.parse(body).Title + "\nYear released: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry Produced:  " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors + "\n", function(err) {
+
+        // If an error was experienced we say it.
+        if (err) {
+          console.log(err);
+        }
+
+        // If no error is experienced, we say nothing
+        else {
+        }
+
 			});
 
+      });
 
 }
 
 
 else if(command === "do-what-it-says"){
+
+
+appendCommand(command);
       fs.readFile("./random.txt", "utf8", function(error, data) {
 
       // If the code experiences any errors it will log the error to the console.
